de38e3d5acc77186ba1b84f2257cf463262bac79
Jhoann <64670851+Jansooxx@users.noreply.github.com>
2023-04-26 19:03:25 +0000

    Ajustes contador simple sencillo

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,86 @@
 import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
 //create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+const Home = (props) => {
+
+  return (
+    
+    <>
+    
+    
+        <div className="text-center d-flex justify-content-center bg-dark">
+
+        <div className="card bg-dark">
+            <div className="card-body">
+              <div className="text-white me-4 border-white fs-1 text-center w-100">
+                {props.unidad}
+              </div>
+            </div>
+          </div>
+
+          <div className="card bg-dark">
+            <div className="card-body">
+              <div className="text-white me-4 border-white fs-1 text-center w-100">
+                {props.decena}
+              </div>
+            </div>
+          </div>
+
+          <div className="card bg-dark">
+            <div className="card-body">
+              <div className="text-white me-4 border-white fs-1 text-center w-100">
+                {props.centena}
+              </div>
+            </div>
+          </div>
+
+          <div className="card bg-dark">
+            <div className="card-body">
+              <div className="text-white me-4 border-white fs-1 text-center w-100">
+                {props.mil}
+              </div>
+            </div>
+          </div>
+
+          <div className="card bg-dark">
+            <div className="card-body">
+              <div className="text-white me-4 border-white fs-1 text-center w-100">
+                {props.diezmil}
+              </div>
+            </div>
+          </div>
+
+          <div className="card bg-dark">
+            <div className="card-body">
+              <div className="text-white text-center me-4 border-dark fs-1 text-center w-100">
+                {props.cienmil}
+              </div>
+            </div>
+          </div>
+
+
+          <div className="card bg-dark">
+            <div className="card-body">
+              <div className="text-white me-4 fs-1">
+                <i className="far fa-clock"></i>
+              </div>
+            </div>
+          </div>
+
+          
+          
+
+          
+
+          
+
+        
+         
+          
+
+
+        </div>
+    </>
+  );
 };
 
 export default Home;
--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,3 @@
-//import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
 
@@ -7,6 +6,43 @@ import "../styles/index.css";
 
 //import your own components
 import Home from "./component/home.jsx";
+
+
+const nums = [0, 0, 0, 0, 0, 0];
+
+setInterval(function() {
+  for (let i = nums.length - 1; i >= 0; i--) {
+    if (nums[i] === 9) {
+      nums[i] = 0;
+    } else {
+      nums[i]++;
+      break;
+    }
+
+      if (num6 === 2 && num5 === 4) {
+        num = 0;
+        num2 = 0;
+        num3 = 0;
+        num4 = 0;
+        num5 = 0;
+        num6 = 0
+    }
+  }
+
+
+  ReactDOM.render(
+    <Home
+      unidad= {nums[0]}
+      decena= {nums[1]}
+      centena={nums[2]}
+      mil=    {nums[3]}
+      diezmil={nums[4]}
+      cienmil={nums[5]}
+    />,
+    document.querySelector("#app")
+  );
+}, 0.1);
+
 
 //render your react application
 ReactDOM.render(<Home />, document.querySelector("#app"));
